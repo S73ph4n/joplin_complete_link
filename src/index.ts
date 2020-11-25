1f75e869e189b062234b9d417b0bca380b518623
S73ph4n <stephanpissot@gmail.com>
2020-11-25 19:03:34 +0100

    Added popup to select choices

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ joplin.plugins.register({
 			label: 'Complete the selection to a note title (if possible).',
 			iconName: 'fas fa-angle-double-right',
 			execute: async () => {
+				const dialogs = joplin.views.dialogs;
 				const notes = (await joplin.data.get(['notes']));
 				const currentNote = await joplin.workspace.selectedNote();	
 				const selectedText = (await joplin.commands.execute('selectedText') as string);
@@ -19,23 +20,33 @@ joplin.plugins.register({
 					//Check if note already exists
 					var idNotes = [];
 					var titleNotes = [];
-					var choiceList = "";
+					var choiceList = [];
 					for (let i in notes.items){
 						//TODO : do a fuzzy search instead
 						if (notes.items[i].title.toLowerCase().slice(0, len_selection) === selectedText.toLowerCase()){
 							idNotes.push(notes.items[i].id);
 							titleNotes.push(notes.items[i].title);
-							choiceList += notes.items[i].title + "\n";
+							choiceList.push({id : notes.items[i].id, title: notes.items[i].title});
 						}
 
 					}
-					var choice = prompt("Please choose :\n" + choiceList, "0");
-					const backlink = 'Linked from [' + currentNote.title + '](:/' + currentNote.id + ')';
-					const bodyLinkedNote = (await joplin.data.get(['notes', idNotes[choice].toString()], { fields: ['body'] })).body;
-					const newBodyLinkedNote = bodyLinkedNote + "\n" + backlink;
-					await joplin.data.put(['notes', idNotes[choice].toString()], null, { body: newBodyLinkedNote });
+					//var choice = prompt("Please choose :\n" + choiceList, "0");
 
-					const linkToNewNote = '[' + titleNotes[choice] + '](:/' + idNotes[choice] + ')';
+					const handle = await dialogs.create();
+					await dialogs.setHtml(handle, "Please choose");
+					console.info(choiceList);
+					await dialogs.setButtons(handle, choiceList);
+
+					const choice = await dialogs.open(handle);
+
+					const backlink = 'Linked from [' + currentNote.title + '](:/' + currentNote.id + ')';
+					const linkedNote = (await joplin.data.get(['notes', choice.toString()], { fields: ['title','body'] }));
+					const titleLinkedNote = linkedNote.title;
+					const bodyLinkedNote = linkedNote.body;
+					const newBodyLinkedNote = bodyLinkedNote + "\n" + backlink;
+					await joplin.data.put(['notes', choice.toString()], null, { body: newBodyLinkedNote });
+
+					const linkToNewNote = '[' + titleLinkedNote + '](:/' + choice + ')';
 
 					await joplin.commands.execute('replaceSelection', linkToNewNote);
 				}
